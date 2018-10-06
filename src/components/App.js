a04c07dd77481a24170d86ac7fffdd5209549ebc
liubakozyr <liubakozyr@gmail.com>
2018-10-06 19:42:17 +0300

    my-blog-app initial commit

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+
+	render() {
+		return (
+		<div className="app">
+			my-blog-app
+		</div>
+		);
+	}
+}
+
+export default App;
+
