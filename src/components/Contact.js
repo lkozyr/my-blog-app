f7cab1d8d45a7e849135f711187bf188d84b4c4a
liubakozyr <liubakozyr@gmail.com>
2018-10-24 22:00:34 +0300

    Corrected links in contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,17 +12,17 @@ const Contact = (props) => {
             <h2>Contact me</h2>
             <ul>
                 <li>
-                    <a href="https://www.facebook.com/">
+                    <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
                         <img src={fbImage} alt="Facebook" />
                     </a>
                 </li>
                 <li>
-                    <a href="https://twitter.com/">
+                    <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
                         <img src={twImage} alt="Twitter" />
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/">
+                    <a href="https://github.com/" target="_blank" rel="noopener noreferrer">
                         <img src={ghImage} alt="Github" />
                     </a>
                 </li>
