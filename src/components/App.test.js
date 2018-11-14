41070fbe674ffbce76b0de560b10c740c1397b5f
liubakozyr <liubakozyr@gmail.com>
2018-11-14 20:53:38 +0200

    Added basic unit tests for <App /> component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it ('Renders <App /> component properly', () => {
+    const component = shallow(<App />);
+
+    expect(component).toHaveLength(1);
+
+    expect(Object.keys(component.props())).toContain('className');
+    expect(Object.keys(component.props())).toContain('children');
+
+    expect(component.props()['className']).toBe('app');
+    expect(component.props()['children'].length).toBe(3);
+  })
+  
+})
+
